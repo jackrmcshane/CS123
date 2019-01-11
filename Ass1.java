616336002890a595039fa7bf79eb8316d83bb361
jackrmcshane <jackrmcshane@gmail.com>
2019-01-10 19:20:59 -0600

    Homework 1

---
+++ Ass1.java
@@ -0,0 +1,28 @@
+/**
+Course: CS 123
+Name: Jack McShane
+Assignment: 1
+*/
+
+public class Ass1{
+  public static void main( String[] args ){
+    for( int i = 1; i < 6; i++ ){ System.out.println( i + ": Jack McShane" ); }
+
+    System.out.println();
+
+    System.out.println( "     **************" );
+    System.out.println( "    *            **" );
+    System.out.println( "   *            * *" );
+    System.out.println( "  *            *  *" );
+    System.out.println( " *            *   *" );
+    System.out.println( "**************    *" );
+    System.out.println( "*            *    *" );
+    System.out.println( "*            *    *" );
+    System.out.println( "*            *    *" );
+    System.out.println( "*            *   *" );
+    System.out.println( "*            *  *" );
+    System.out.println( "*            * *" );
+    System.out.println( "*            **" );
+    System.out.println( "**************" );
+  }
+}
