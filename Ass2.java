87b006a280a8984c16c214af9ec6a0c1b1c7bc30
Asimov <jackrmcshane@gmail.com>
2019-01-16 13:04:48 -0600

    Assignment 2 finished product

--- Ass2.java
+++ Ass2.java
@@ -13,32 +13,44 @@ public class Ass2{
     int size;
     int[] brokenInteger;
 
-    System.out.println( "Enter and integer between 0 and 99999: " );
+    System.out.print( "Enter and integer between 0 and 99999: " );
     numberInput = userInput.nextInt();
 
-    if( !isValidNumber( numberInput ) ){
-      System.out.println( "Fatal error. Number must be an integer between 0 and 99999. Exiting program." );
-      break;
-    }else{
-      size = sizeOf( input );
+    if( !isValidInput( numberInput ) ){ System.out.print( "Fatal error. Number must be an integer between 0 and 99999." ); }
+    else{
+      size = sizeOf( numberInput );
       brokenInteger = new int[ size ];
-      brokenInteger = separateNumber( input, size );
+      brokenInteger = separateNumber( numberInput, size );
 
-      System.out.print( "The number you entered can be separated as such:" );
-      for( int i = size; i >= 0; --i ){ System.out.print( " " + brokenInteger[ i ] ); }
+      System.out.println( "The number you entered can be separated as such:" );
+      for( int i = 0; i < size; i++ ){ System.out.print( " " + brokenInteger[ i ] ); }
     }
+
+    System.out.println( "\nProgram terminated." );
   }
 
-  public static int sizeOf( int input ){
-    if( input < 10 ){ retrun 1; }
-    else if( )
+  public static int sizeOf( int numberInput ){
+    if( numberInput < 10 ){ return 1; }
+    else if( numberInput < 100 ){ return 2; }
+    else if( numberInput < 1000 ){ return 3; }
+    else if( numberInput < 10000 ){ return 4; }
+    else{ return 5; }
   }
 
-  public static int[] separateNumber(int input, int size ){
+  public static int[] separateNumber(int numberInput, int size ){
+    int[] separatedNumber = new int[ size ];
 
+    for( int j = size - 1; j >= 0; j-- ){
+      separatedNumber[ j ] = numberInput % 10;
+      numberInput /= 10;
+    }
+
+    return separatedNumber;
   }
 
-  public static boolean isValidInput(){
-
+  public static boolean isValidInput( int numberInput){
+    if( numberInput < 0 ){ return false; }
+    else if( numberInput > 99999 ){ return false; }
+    else{ return true; }
   }
 }
